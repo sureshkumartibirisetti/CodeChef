fb50a75d4e3cbe1427e1e7512737895af14a277c
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-11 13:18:03 +0530

    Easy

---
+++ Janmansh and Assignments.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner (System.in);
+		int a=in.nextInt();
+		while(a-->0){
+		    int x=in.nextInt();
+		    if ((x+3)<=10){
+		        System.out.println("Yes");
+		    }else{
+		        System.out.println("No");
+		    }
+		}
+
+	}
+}
