9ff03f4bdbf94a083e287bc4299675094df51984
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-11 09:20:50 +0530

    Create Bull or Bear.java

---
+++ Bull or Bear.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int testCase=in.nextInt();
+		for (int i=0;i<testCase;i++){
+		    int stock=in.nextInt();
+		    int sold=in.nextInt();
+		    if(stock>sold){
+		        System.out.println("LOSS");
+		    }else if(stock==sold){
+		        System.out.println("NEUTRAL");
+		    }else{
+		        System.out.println("PROFIT");
+		    }
+		}
+
+	}
+}
