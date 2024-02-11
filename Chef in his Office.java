e6667904d7fcd13e75fefdde002d7ef72392ba7b
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-11 22:31:37 +0530

    Easy

---
+++ Chef in his Office.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int i=in.nextInt();
+		while (i-->0){
+		    int a=in.nextInt();
+		    int b=in.nextInt();
+		    System.out.println((a*4)+b);
+		}
+
+	}
+}
