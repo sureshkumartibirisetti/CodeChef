e1098c6df83e76041ea83fc7eed0a8e499f93578
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-11 09:12:27 +0530

    Create Saving Taxes.java

---
+++ Saving Taxes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		for (int i=0;i<t;i++){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    System.out.println(a-b);
+		}
+
+	}
+}
