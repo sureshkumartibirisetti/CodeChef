ce704868436aa3d86b01d4136a2ecaf9dca86b8f
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-12 18:54:26 +0530

    Create 404 Not Found.java

---
+++ 404 Not Found.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int i=in.nextInt();
+		if (i==404){
+		    System.out.println("NOT FOUND");
+		}else{
+            System.out.println("FOUND");
+		}
+	}
+}
