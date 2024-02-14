f92cc85e61a52c564f1aa911ea40d126a37e6e83
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-14 21:34:14 +0530

    Easy

---
+++ Shopping Change.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int T=in.nextInt();
+		while (T-->0){
+		    int a=in.nextInt();
+		    System.out.println(100-a);
+		}
+
+	}
+}
