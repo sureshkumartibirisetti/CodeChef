7512ed963036ce8215ba0363b0d7806307e8e2fc
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-14 21:37:31 +0530

    Easy

---
+++ Janmansh and Coins.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int T=in.nextInt();
+		while(T-->0){
+		    int a=in.nextInt();
+		    int b=in.nextInt();
+		    System.out.println((a*10)+(b*5));
+		}
+
+	}
+}
