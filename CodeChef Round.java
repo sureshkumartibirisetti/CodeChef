de26d70626898c304ce6e9a25317f92afad6f652
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-14 09:48:22 +0530

    Basic

---
+++ CodeChef Round.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		String ans=(n==4)?"YES":"NO";
+		System.out.println(ans);
+
+	}
+}
