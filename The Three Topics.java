ff7abc0d2800e49454bc826691b5f363d4f53b54
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-02-17 05:37:08 +0530

    Easy

---
+++ The Three Topics.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner (System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int c=in.nextInt();
+		int x=in.nextInt();
+		if ((a==x)||(b==x)||(c==x)){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+
+	}
+}
