126ce963481840f8d9529b490ece19559205a9ba
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-02-19 22:31:10 +0530

    Easy

---
+++ Sum OR Difference.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int n1=in.nextInt();
+		int n2=in.nextInt();
+		if (n1>n2){
+		    System.out.println(n1-n2);
+		}else{
+		    System.out.println(n1+n2);
+		}
+
+	}
+}
