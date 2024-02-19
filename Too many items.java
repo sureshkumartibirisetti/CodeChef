d1757b2a897d27114038ab75343c1b0da77b108d
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-02-19 22:35:04 +0530

    Easy

---
+++ Too many items.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int T=in.nextInt();
+		while(T-->0){
+		    int a=in.nextInt();
+		    if(a%10==0){
+		        System.out.println(a/10);
+		    }else{
+		        System.out.println((a/10)+1);
+		    }
+		}
+
+	}
+}
