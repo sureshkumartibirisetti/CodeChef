0507fcb220426522c0815aceb375a738e8a1223c
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-02-20 05:26:00 +0530

    Easy

---
+++ Helping Chef.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        while (T-- > 0) {
+            int a = in.nextInt();
+            if (10 > a) {
+                System.out.println("Thanks for helping Chef!");
+            } else {
+                System.out.println("-1");
+            }
+        }
+
+    }
+}
