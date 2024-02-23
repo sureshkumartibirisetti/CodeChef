9fe027d7c8dbebcbf7b4cd4312b502a27f1f385e
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-02-23 11:40:14 +0530

    Easy

---
+++ Watching Movies at 2x.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner (System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		System.out.println((a-b)+(b/2));
+
+	}
+}
