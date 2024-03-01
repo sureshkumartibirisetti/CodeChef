18cc26de6eac28d0f0980f7e2243ba84005814f4
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-03-01 21:59:39 +0530

    Easy

---
+++ Maximum Length Even Subarray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int T=in.nextInt();
+		while(T-->0){
+		    int a=in.nextInt();
+		    int s=0;
+		    for(int i=1;i<=a;i++){
+		        s+=i;
+		    }
+		    if(s%2==0){
+		        System.out.println(a);
+		    }else{
+		        System.out.println(a-1);
+		    }
+		}
+
+	}
+}
