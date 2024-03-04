5078493e10bff63840a11792a98f2548ccab4b71
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-03-04 11:45:11 +0530

    Easy

---
+++ Make N Zero.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int T=in.nextInt();
+		while(T-->0){
+		    int a=in.nextInt();
+		    
+		    if(a==1 || a==2 || a==5){
+		        System.out.println("NO");
+		    }else{
+		        System.out.println("YES");
+		    }
+		}
+
+	}
+}
