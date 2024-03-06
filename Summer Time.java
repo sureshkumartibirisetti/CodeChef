118fa4a9c8d9b0fd95ee01547d86b59e3e1ec215
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-03-06 22:01:14 +0530

    Easy

---
+++ Summer Time.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		if(35<a){
+		    System.out.println("YES");
+		}else{
+		    System.out.println("NO");
+		}
+
+	}
+}
