7e72716d8c4c10ddfc33608c5f7ee8d9fbfc2fe4
SURESH KUMAR <20mh1a04i2@acoe.edu.in>
2024-03-06 22:01:55 +0530

    Easy

---
+++ Cricket Tournament.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int T=in.nextInt();
+		while(T-->0){
+		    int N=in.nextInt();
+		    int M=in.nextInt();
+		    if((N-1)>=M){
+		        System.out.println("YES");
+		    }else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
